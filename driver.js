0df68a75dc8cfe62c4b104bdaa4b704cd872db5d
Yingcong Zhu <zyu.bupt@gmail.com>
2017-09-05 22:16:17 +0800

    run very well version

--- driver.js
+++ driver.js
@@ -1,115 +1,138 @@
 'use strict';
-let http = require('http');
-// var Promise = require('bluebird');
-let cheerio = require('cheerio');
-let fs = require('fs')
 
-const Config = {
-    startPage: 1, //开始页码
-    endPage: 1, //结束页码，不能大于当前图片类型总页码
-    downloadImg: true, //是否下载图片到硬盘,否则只保存Json信息到文件
-    downloadConcurrent: 10, //下载图片最大并发数
-    currentImgType: "scy" //当前程序要爬取得图片类型,取下面AllImgType的Key。
-};
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const cheerio = require('cheerio')
+const async = require('async')
 
-// console.log(pageIds)
-const AllImgType = { //网站的图片类型
-    ecy: "http://tu.hanhande.com/ecy/ecy_", //二次元   总页码: 50
-    scy: "http://tu.hanhande.com/scy/scy_", //三次元   总页码: 64
-    cos: "http://tu.hanhande.com/cos/cos_", //cosPlay 总页码: 20
-};
-let albums = [];
-for(let i = Config.startPage; i<= Config.endPage; i++){
-    albums.push('http://tu.hanhande.com/cos/cos_' + i + '.shtml');
+let albumURL = 'http://tu.hanhande.com/cos/7042311.shtml'
+let pageList = ['http://tu.hanhande.com/scy/scy_7.shtml','http://tu.hanhande.com/scy/scy_8.shtml']
+let albumList = ['http://tu.hanhande.com/cos/7042311.shtml', 'http://tu.hanhande.com/cos/7042221.shtml']
+
+function saveData(pics){
+    let folder = 'data'
+    if(!fs.existsSync(folder)){
+        fs.mkdirSync(folder)
+    }
+    fs.writeFile(folder + '/data.json', JSON.stringify(pics),function (err) {
+        if(err){
+            return console.log(err)
+        }
+        console.log('Data Saved')
+    })
 }
-console.log(albums);
 
-let getAlbumsAsync = function(url){
-    return new Promise(function (resolve, reject) {
-        console.log('Crawling albums: ' );
-        let albums = [];
-        http.get(url, function(res) {
-            let content = '';
-            res.on('data', function (chunk) {
-                content += chunk;
-            });
-            res.on('end', function(){
-                let $ = cheerio.load(content);
-                $('.picList p a').each(function(){
-                    let album = $(this);
-                    albums.push({
-                        albumURL: album.attr('href'),
-                        imgList: []
-                    });
-                    // console.log(album.attr('href'));
+function downloadIMG(urls) {
+    let folder = 'img'
+    if(!fs.existsSync(folder)){
+        fs.mkdirSync(folder)
+    }
+    async.mapSeries(urls, function (url, callback) {
+        http.get(url,function (res) {
+            let data = ''
+            res.setEncoding('binary')
+            console.log('downloading: '+ path.basename(url))
+            res.on('data',function (chunk) {
+                data += chunk
+            })
+            res.on('end',function () {
+                fs.writeFile(folder + '/' + path.basename(url),data,'binary',function (err){
+                    if(err){
+                        return console.log(err)
+                    }
+                    let callbackTime = (Math.random() + '0').substr(2,3) * 1
+                    // console.log(callbackTime)
+                    console.log('img downloaded: '+ path.basename(url))
+                    setTimeout(function () {
+                        callback(null, url)
+                    }, callbackTime)
+
                 })
-                resolve(albums);
             })
-        }).on('error', (e)=>{
-            console.log('error: '+ e.message);
-            reject(e);
-        });
-    });
-};
+        }).on('error',function (err) {
+            console.log(err)
+        })
+    })
 
-let getImageListAsync = function (albumsList) {
+}
+
+let getAlbumAsync = function (url) {
     return new Promise(function (resolve, reject) {
-        console.log('Crawling pics: ' + albumsList);
-        let url = albumsList;
-        http.get(url, function(res) {
-            let content = '';
-            let imgList = []
+        console.log("Crawling Page: " + path.basename(url))
+        http.get(url, function (res) {
+            let html = ''
+            let albums = []
             res.on('data', function (chunk) {
-                content += chunk;
-            });
-            res.on('end', function(){
-                let $ = cheerio.load(content);
-                $('#picLists a img').each(function(){
-                    let picList = $(this);
-                    console.log(picList.attr('src'));
-                    imgList.push(picList.attr('src'));
-                });
-                let albums = {
-                    albumURL : url,
-                    imgList : imgList
-                }
-                // console.log(albumsList);
-                // console.log(albums)
-                resolve(albums);
+                html += chunk
             })
-        }).on('error', (e)=>{
-            console.log('error: '+ e.message);
-            reject(e);
-        });
-
-    });
-};
-
-let test = Promise
-    .all(albums.map(getAlbumsAsync))
-    .then(function(data){
-        // console.log(data[0][0].albumURL)
-        let albumList = [];
-        data.map(function(page){
-            let test_page = page.slice(1,6)
-            test_page.map(function(album){
-                albumList.push(album.albumURL);
+            res.on('end', function () {
+                let $ = cheerio.load(html)
+                $('.picList p a').each(function(){
+                    let album = $(this).attr('href')
+                    albums.push(album)
+                })
+                resolve(albums)
             })
         })
-        return Promise.all(albumList.map(getImageListAsync));
-        // return Promise.all(data.map(getImageListAsync))
     })
-    .then(function (albumList) {
-        let folder = `json-${Config.currentImgType}-${Config.startPage}-${Config.endPage}`
-        fs.mkdirSync(folder);
-        let filePath = `./${folder}/${Config.currentImgType}-${Config.startPage}-${Config.endPage}.json`;
-        fs.writeFileSync(filePath, JSON.stringify(albumList));
-        // console.log(albumList);
+}
+
+let allData = []
+
+let getImageAsync = function (url) {
+    return new Promise(function (resolve, reject) {
+        console.log("Crawling Album: " + path.basename(url))
+        http.get(url, function (res) {
+            let html = ''
+            let picsList = []
+            res.on('data',function(chunk){
+                html += chunk
+            })
+            res.on('end',function(){
+                let $ = cheerio.load(html)
+                $('#picLists a img').each(function(){
+                    let picURL = $(this).attr('src')
+                    picsList.push(picURL)
+                    // downloadIMG(picURL)
+                })
+                allData.push(picsList)
+                resolve(picsList)
+                saveData(allData)
+            }).on('error', function (err) {
+                console.log(error)
+            })
+        })
+    })
+}
+
+let test = Promise.all(pageList.map(getAlbumAsync))
+    .then(function (albums) {
+        let allAlbums = []
+        albums.forEach(function (elem, index) {
+            elem.forEach((function (item, index) {
+                allAlbums.push(item)
+            }))
+        })
+        // allAlbums = allAlbums.slice(0,20)
+        // console.log(allAlbums)
+        return Promise.all(allAlbums.map(getImageAsync))
+    })
+    .then(function (data) {
+        let allPics = []
+        // console.log(albumLength)
+        data.forEach(function (elem, index) {
+            elem.forEach(function (item, index) {
+                allPics.push(item)
+            })
+        })
+        downloadIMG(allPics)
+        /*        async.mapSeries(allPics, function (pic, callback) {
+                    console.log('downloading from: ' + path.basename(pic))
+                    downloadIMG(pic)
+                    setTimeout(function () {
+                        callback(null, pic)
+                    },1000)
+                })*/
     })
 
-/*
-let actions = albums.map(getAlbumsAsync);
-let results = Promise.all(actions);
-results.then(function(data){
-    console.log(data);
-})*/
