0dfbe8c19cb054091d6171e998231e78b1a4d9d3
Yingcong Zhu <zyu.bupt@gmail.com>
2017-09-08 20:35:30 +0800

    official release, add some trivial comments

--- driver.js
+++ driver.js
@@ -6,18 +6,27 @@ const path = require('path')
 const cheerio = require('cheerio')
 const async = require('async')
 
-const CONFIG = {
-    startPage: 6,   //开始页码
-    endPage: 8,     //结束页码，不能小于开始页码
+const CONFIG = {        //能改变程序执行结果的参数都在这里
+    startPage: 9,   //开始页码
+    endPage: 9,     //结束页码，不能小于开始页码
     currentImgType: 'scy',  //要下载的图片类型，详见下面的IMGTYPE
     download: true,     //是否下载图片，否的话只保存JSON文件
     downloadDelay: 1    //每次下载之间的最大可能间隙
 }
 
 const IMGTYPE = {
-    ecy: 'http://tu.hanhande.com/ecy/ecy_',
-    scy: 'http://tu.hanhande.com/scy/scy_',
-    cos: 'http://tu.hanhande.com/cos/cos_'
+    ecy: 'http://tu.hanhande.com/ecy/ecy_',     //二次元
+    scy: 'http://tu.hanhande.com/scy/scy_',     //三次元
+    cos: 'http://tu.hanhande.com/cos/cos_'      //COS
+}
+
+const folderJSON = 'data'
+if(!fs.existsSync(folderJSON)){     //新建一个文件夹之前你首先也要判断
+    fs.mkdirSync(folderJSON)
+}
+const folderIMG = 'img'
+if(!fs.existsSync(folderIMG)){
+    fs.mkdirSync(folderIMG)
 }
 
 let jsonFileName = '/' + CONFIG.currentImgType + '-' + CONFIG.startPage + '-' + CONFIG.endPage + '.json'
@@ -28,12 +37,8 @@ for(let i = CONFIG.startPage; i <= CONFIG.endPage; i++){
 }
 
 function saveData(pics){
-    let folder = 'data'
-    if(!fs.existsSync(folder)){     //新建一个文件夹之前你首先也要判断
-        fs.mkdirSync(folder)
-    }
     // fs.writeFile(folder + '/data.json', JSON.stringify(pics),function (err) {
-    fs.writeFile(folder + jsonFileName, JSON.stringify(pics),function (err) {
+    fs.writeFile(folderJSON + jsonFileName, JSON.stringify(pics),function (err) {
         if(err){
             return console.log(err)
         }
@@ -45,10 +50,6 @@ function saveData(pics){
 // let concurrency = 0
 
 function downloadIMG(urls) {
-    let folder = 'img'
-    if(!fs.existsSync(folder)){
-        fs.mkdirSync(folder)
-    }
     // async.mapLimit(urls, CONCUR, function (url, callback) {      //一并发就会炸
     async.mapSeries(urls, function (url, callback) {
         http.get(url,function (res) {
@@ -60,7 +61,7 @@ function downloadIMG(urls) {
                 data += chunk
             })
             res.on('end',function () {
-                fs.writeFile(folder + '/' + path.basename(url),data,'binary',function (err){    //保存的图片文件名取URL最后一个斜杠之后的信息
+                fs.writeFile(folderIMG + '/' + path.basename(url),data,'binary',function (err){    //保存的图片文件名取URL最后一个斜杠之后的信息
                     if(err){
                         return console.log(err)
                     }
@@ -78,7 +79,6 @@ function downloadIMG(urls) {
             console.log(err)
         })
     })
-
 }
 
 let getAlbumAsync = function (url) {
